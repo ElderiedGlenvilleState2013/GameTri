ab50f20ad896f828d3c8ca224ab6cacf896144de
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-09-04 20:01:22 -0400

    aded mobile app

---
+++ iOSGameTri/GameTri/GameTri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameTri
+//
+//  Created by McKinney family  on 9/4/19.
+//  Copyright © 2019 FasTek Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
